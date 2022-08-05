bbe97a3ad34b731b2cb0d1c48fb3695159e3b8b3
Rahul Kumar <rahulcsofficial@gmail.com>
2022-08-06 00:49:56 +0530

    Initialized repo with all dependencies

---
+++ src/main/java/com/devrahul/costanalyzer/CostanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.devrahul.costanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostanalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devrahul/costanalyzer/CostanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrahul.costanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
